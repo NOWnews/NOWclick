eb1b7a5069894e074f9b9b639b71befbe4a35d58
esbb48 <esbb48@gmail.com>
2016-12-06 15:20:16 +0800

    Add error catch

--- api/controllers/announce/list.js
+++ api/controllers/announce/list.js
@@ -1,31 +1,41 @@
 module.exports = async (req, res, next) => {
-    let today = new Date();
-    let announces = await db.Announce.findAll({
-        attributes: [
-            'content',
-            'endTime',
-            'startTime',
-            'type',
-        ],
-        where: {
-            actived: true,
-            $or: [
-                {
-                    startTime: {
-                        $gte: today
+    try {
+
+        let today = new Date();
+
+        let announces = await db.Announce.findAll({
+            attributes: [
+                'content',
+                'endTime',
+                'startTime',
+                'type',
+            ],
+            where: {
+                actived: true,
+                $or: [
+                    {
+                        startTime: {
+                            $gte: today
+                        },
                     },
-                },
-                {
-                    startTime: {
-                        $lte: today
-                    },
-                    endTime: {
-                        $gte: today
-                    },
-                }
-            ]
-        },
-        order: 'startTime ASC',
-    });
-    return res.json(announces);
+                    {
+                        startTime: {
+                            $lte: today
+                        },
+                        endTime: {
+                            $gte: today
+                        },
+                    }
+                ]
+            },
+            order: 'startTime ASC',
+        });
+
+        return res.json(announces);
+
+    } catch (e) {
+
+        return next(e);
+
+    }
 };
--- api/controllers/version/one.js
+++ api/controllers/version/one.js
@@ -1,20 +1,30 @@
 module.exports = async (req, res, next) => {
-    let today = new Date();
-    let { deviceType } = req.query;
+    try {
 
-    let version = await db.Version.findOne({
-        attributes: [
-            'apkUrl',
-            'publishedDate',
-            'version',
-        ],
-        where: {
-            deviceType,
-            publishedDate: {
-                $lte: today,
+        let today = new Date();
+        let { deviceType } = req.query;
+
+        let version = await db.Version.findOne({
+            attributes: [
+                'apkUrl',
+                'publishedDate',
+                'version',
+            ],
+            where: {
+                deviceType,
+                publishedDate: {
+                    $lte: today,
+                },
             },
-        },
-        order: 'publishedDate DESC'
-    });
-    return res.json(version);
+            order: 'publishedDate DESC'
+        });
+
+        return res.json(version);
+
+    } catch (e) {
+
+        return next(e);
+
+    }
+
 };
