d590fa1a633897ccec26b87e17983f066651bea9
esbb48 <esbb48@gmail.com>
2016-12-07 16:11:36 +0800

    Update create device

--- api/controllers/device/create.js
+++ api/controllers/device/create.js
@@ -1,6 +1,9 @@
 module.exports = async (req, res, next) => {
     try {
-        let device = await db.Device.create(req.body);
+        let params = req.body;
+        params.createdBy = config.superAdminId;
+        params.updatedBy = config.superAdminId;
+        let device = await db.Device.create(params);
         return res.json(device);
     } catch (e) {
         return next(e);
