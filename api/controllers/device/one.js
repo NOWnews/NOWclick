84bed8e2e964711db7ad29122643e9def96e50a7
esbb48 <esbb48@gmail.com>
2016-12-07 17:09:54 +0800

    Update find version

--- api/controllers/device/one.js
+++ api/controllers/device/one.js
@@ -1,7 +1,17 @@
 module.exports = async (req, res, next) => {
     try {
         let { deviceId } = req.params;
-        let device = await db.Device.findById(deviceId);
+        let device = await db.Device.findOne({
+            where: {
+                id: deviceId,
+                actived: true,
+            },
+            attributes: [
+                'id',
+                'identifier',
+                'type',
+            ]
+        });
         return res.json(device);
     } catch (e) {
         return next(e);
