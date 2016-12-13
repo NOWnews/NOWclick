1e6b01d34d458d12232dc3a37c1caaa66ba21302
WebberWu <bird23074035@gmail.com>
2016-12-13 10:48:20 +0800

    update

--- api/controllers/device/create.js
+++ api/controllers/device/create.js
@@ -1,14 +1,15 @@
 module.exports = async (req, res, next) => {
     try {
         let params = req.body;
-        params.actived = true;
-        params.createdBy = config.superAdminId;
-        params.updatedBy = config.superAdminId;
-        let optionObj = {
-            where: {identifier: params.identifier},
-            defaults: params
+        let result = await db.Device.find(params);
+
+        // 如果找不到這筆資料才建立
+        if (!result) {
+            params.actived = true;
+            params.createdBy = config.superAdminId;
+            params.updatedBy = config.superAdminId;
+            result = await db.Device.create(params);
         }
-        let result = await db.Device.findOrCreate(optionObj);
         return res.json({ result });
     } catch (e) {
         return next(e);
