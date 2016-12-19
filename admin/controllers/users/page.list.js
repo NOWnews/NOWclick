d164b30f8803598af6fc35cf0e101fd8085fcbc9
Simon Sun <blackghost0118@gmail.com>
2016-12-19 16:35:43 +0800

    使用者列表頁串接完成

--- admin/controllers/users/page.list.js
+++ admin/controllers/users/page.list.js
@@ -1,13 +1,41 @@
+import Debug from 'debug';
+const debug = Debug('NOWott-admin:controllers:users:page.list');
+
+import moment from 'moment-timezone';
 
 module.exports = async (req, res, next) => {
 
     try{
-        let result = await new Promise((resolve, reject) => {
-            return resolve('controllers/users/page.list.js');
+
+        let users = await db.User.findAll({
+            where: {},
+            raw: true
         });
 
-        console.log(result);
-        return res.render('users/list.html');
+        // 正規化資料
+        let formatUsers = _.map(users, (user) => {
+            user.createdAt = moment(user.createdAt).tz('Asia/Taipei').format('YYYY/MM/DD HH:mm');
+            user.updatedAt = moment(user.updatedAt).tz('Asia/Taipei').format('YYYY/MM/DD HH:mm');
+
+            if(user.gender === 'MALE') {
+                user.gender = '男';
+            }
+
+            if(user.gender === 'FEMALE') {
+                user.gender = '女';
+            }
+
+            if(!user.gender) {
+                user.gender = '無';
+            }
+
+            return user;
+        });
+
+        debug('result = %j', users);
+        return res.render('users/list.html', {
+            users
+        });
     }
     catch(err) {
         return next(err);
