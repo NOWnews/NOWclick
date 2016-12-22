249cfc85f100dc061f56c329a965cec1cfbe6119
wayne1025 <wayne1025.tw@hotmail.com>
2016-12-22 10:40:13 +0800

    增加admin create檔案

---
+++ admin/controllers/admins/action.create.js
@@ -0,0 +1,56 @@
+/*
+ * 新增管理員
+ */
+
+import Debug from 'debug';
+const debug = Debug('NOWott-admin:controllers:users:action.create');
+
+import { getHashedPassword } from '../../../libs';
+
+module.exports = async (req, res, next) => {
+
+    let { username, password, phone, email, address, gender, memo } = req.body;
+
+    try {
+
+        let hashedPassword = getHashedPassword(password);
+
+        let user = await db.User.findOne({
+                where: {
+                    username,
+                    hashedPassword
+                }
+            });
+
+        debug('user = %j', user);
+
+        if(user) {
+            throw new Error('使用者已經存在');
+        }
+
+        let options = {
+            username,
+            // password 是一個虛擬欄位，他會幫你把 pwd hash，然後存入到 hashedPassword 這個欄位
+            password,
+            phone,
+            email,
+            address,
+            gender,
+            memo
+        };
+
+        options.createdBy = config.superManagerId;
+        options.updatedBy = config.superManagerId;
+
+        debug('options = %j', options);
+
+        let newUser = await db.User.create(options);
+
+        debug('newUser = %j', newUser);
+
+        return res.redirect('/users');
+
+    } catch(err) {
+        return next(err);
+    }
+};
--- admin/controllers/admins/index.js
+++ admin/controllers/admins/index.js
@@ -6,13 +6,18 @@ import pageCreate from './page.create';
 import pageList from './page.list';
 import pageOne from './page.one';
 
+import actionCreate from './action.create';
+import actionUpdate from './action.update';
+
 router.route('/create')
+    .post(actionCreate)
     .get(pageCreate);
 
 router.route('/')
     .get(pageList);
 
 router.route('/:id')
+    .put(actionUpdate)
     .get(pageOne);
 
 module.exports = router;
