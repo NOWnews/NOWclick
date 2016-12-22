0a2a0bf1f3a30c3e455f3b339413743e7a8d06c2
wayne1025 <wayne1025.tw@hotmail.com>
2016-12-22 13:54:25 +0800

    增加列表 及單一頁面拉資料庫資料呈現

--- admin/controllers/managers/page.list.js
+++ admin/controllers/managers/page.list.js
@@ -1,13 +1,28 @@
+import Debug from 'debug';
+const debug = Debug('NOWott-admin:controllers:managers:page.list');
+
+import moment from 'moment-timezone';
 
 module.exports = async (req, res, next) => {
 
     try{
-        let result = await new Promise((resolve, reject) => {
-            return resolve('controllers/managers/page.list.js');
+
+        let managers = await db.Manager.findAll({
+            where: {},
+            raw: true
         });
 
-        console.log(result);
-        return res.render('managers/list.html');
+        // 正規化資料格式
+        _.forEach(managers, (manager) => {
+            manager.createdAt = moment(manager.createdAt).tz('Asia/Taipei').format('YYYY/MM/DD HH:mm');
+            manager.updatedAt = moment(manager.updatedAt).tz('Asia/Taipei').format('YYYY/MM/DD HH:mm');
+
+        });
+
+        debug('result = %j', managers);
+        return res.render('managers/list.html', {
+            managers
+        });
     }
     catch(err) {
         return next(err);
